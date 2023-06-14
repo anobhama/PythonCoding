6e0900e4d0198387430d3ea43085474cb1ff1609
Anobhama <anobhama99.com>
2023-06-14 16:35:55 +0530

    Basic reversal of string

---
+++ ReverseString.py
@@ -0,0 +1,5 @@
+"""that accepts the user's first and last name and prints them in reverse order with a space between them."""
+fname=input("Enter the 1st name: ")
+lname=input("Enter the last name: ")
+print("Ur name is {} {}".format(fname,lname))
+print("Reversed name is {} {}".format(lname,fname))
