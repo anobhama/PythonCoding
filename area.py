9e7b5ed8905cb4239d44dd4905e3810f53908a2b
Anobhama <anobhama99.com>
2023-06-14 16:30:47 +0530

    Area of circle

---
+++ area.py
@@ -0,0 +1,6 @@
+"""calculates the area of a circle based on the radius entered by the user.
+Sample Output :
+r = 1.1
+Area = 3.8013271108436504"""
+r=float(input("Enter the radius "))
+print("Area = ",3.14*r*r)
